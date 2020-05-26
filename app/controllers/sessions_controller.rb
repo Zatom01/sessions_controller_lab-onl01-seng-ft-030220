c1768a90610ab26599fb602f770241a7fc0201a1
Anil Gurung <anil_gurung15@yahoo.com>
2020-05-26 08:29:40 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,17 @@ class SessionsController < ApplicationController
   end
 
   def create
+
+    if params[:name].nil? || params[:name].empty?
+      redirect_to login_path
+    else
+      session[:name] = params[:name]
+      redirect_to '/'
+    end
+    
   end
 
   def destroy
+    session.delete :name
   end
 end
